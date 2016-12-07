df0af611776e8c09b5215d6290ca156704e87da5
Boizard Adrien <11402933@T1700-debian.ig-edu.univ-paris13.fr>
2016-12-07 11:32:25 +0100

    maj

---
+++ Les_hommes_loups/src/Villageois.java
@@ -0,0 +1,22 @@
+
+public class Villageois implements Carte {
+
+	@Override
+	public void description() {
+		System.out.println("Tu es villageois. Tu es du bétail pour les LG. GL HF");
+		
+	}
+
+	@Override
+	public int[] tourNuit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] tourJour() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
